fdf022ffd4f88fc50d97433c1f37e7261bd4d2d6
Rob Wood <rob@rnwood.co.uk>
2019-03-29 19:26:30 +0000

    Try download last docker image and use to seed the docker cache to speed up current build.

---
+++ appveyor-seeddockercache.ps1
@@ -0,0 +1,17 @@
+$project = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/rnwood/smtp4dev/history?recordsnumber=50&branch=$($env:APPVEYOR_REPO_BRANCH)" -Headers $headers  -Method Get
+
+foreach($build in $project.builds) {
+    $builddetail = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/rnwood/smtp4dev/build/$($build.version)" -Headers $headers  -Method Get
+    $jobid = [string] ($builddetail.build.jobs | where-object { $_.name  -eq $jobname} | select-object -First 1 -ExpandProperty jobId)
+
+    if ($jobid) {
+        $artifact = @(Invoke-RestMethod -Uri "https://ci.appveyor.com/api/buildjobs/$jobid/artifacts" -Headers $headers)[0]
+        
+        if ($artifact) {
+            write-host "Found $($artifact.fileName)"
+            Invoke-WebRequest -OutFile $artifact.fileName -Uri "https://ci.appveyor.com/api/buildjobs/$jobid/artifacts/$($artifact.fileName)"
+            docker load "$artifact.fileName"
+            break;
+        }
+    }
+}
